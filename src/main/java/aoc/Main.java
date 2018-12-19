d896fc795f7f563b0e0f9f8ad50ea43f94797c80
Valentin Rochet <val.rochet@gmail.com>
2018-12-19 18:26:42 +0100

    Jour 9 exo 1

--- src/main/java/aoc/Main.java
+++ src/main/java/aoc/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
         try {
-            Day8.exo2();
+            Day9.exo1();
         } catch (Exception e) {
             System.out.println(e);
         }
---
+++ src/main/java/aoc/days/Day9.java
@@ -0,0 +1,79 @@
+package aoc.days;
+
+import aoc.ExoEntryUtils;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Day9 {
+
+    public static void exo1() throws IOException {
+
+        String[] entries = ExoEntryUtils.getEntries(9, 1);
+
+        Integer nbPlayer = Integer.valueOf(entries[0].split(" ")[0]);
+        Integer lastMarble = Integer.valueOf(entries[0].split(" ")[6]);
+
+        System.out.println("Number of players : " + nbPlayer);
+        System.out.println("Last marble value : " + lastMarble);
+
+        int playerTurn = 1;
+
+        int[] marbleGame = new int[]{0, 1};
+        int currentMarblePosition = 1;
+
+        // On stockes les scores dans un tableau et on met tout le monde à 0
+        int[] scores = new int[nbPlayer];
+        Arrays.fill(scores, 0);
+
+        for (int marble = 2; marble <= lastMarble; marble++) {
+
+            if(marble % 23 == 0) {
+                // récupération de la 7eme bille avant et ajout des points
+                currentMarblePosition = currentMarblePosition < 7 ? currentMarblePosition + marbleGame.length - 7 : currentMarblePosition - 7;
+
+                // ajout points 7eme bille et bille à ajouter
+                scores[playerTurn - 1] = scores[playerTurn - 1] + marble + marbleGame[currentMarblePosition];
+
+                // suppression de la bille
+                marbleGame = removeMarble(marbleGame, currentMarblePosition);
+
+            } else {
+                // recherche position nouvelle bille
+                int nextPosition = (currentMarblePosition + 2) % marbleGame.length;
+
+                // ajout bille
+                marbleGame = addMarble(marbleGame, nextPosition, marble);
+                currentMarblePosition = nextPosition;
+            }
+
+            playerTurn = playerTurn == nbPlayer ? 1 : playerTurn + 1;
+        }
+
+        int highestScore = Arrays.stream(scores).max().getAsInt();
+        System.out.println("Highest score : " + highestScore);
+    }
+
+    private static int[] addMarble(int[] marbleGame, int positionNewMarble, int valueMarble) {
+        int[] result = new int[marbleGame.length + 1];
+        System.arraycopy(marbleGame, 0, result, 0, positionNewMarble);
+        System.arraycopy(marbleGame, positionNewMarble, result, positionNewMarble + 1, result.length - positionNewMarble - 1);
+        result[positionNewMarble] = valueMarble;
+        return result;
+    }
+
+    private static int[] removeMarble(int[] marbleGame, int positionMarble) {
+        int[] result = new int[marbleGame.length - 1];
+        System.arraycopy(marbleGame, 0, result, 0, positionMarble);
+        System.arraycopy(marbleGame, positionMarble + 1, result, positionMarble, result.length - positionMarble - 1);
+        return result;
+    }
+
+    public static void exo2() throws IOException {
+
+    }
+}
