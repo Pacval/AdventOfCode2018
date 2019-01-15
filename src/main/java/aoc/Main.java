6cfab04030d586b5bbeded9001b3c14386864432
Valentin Rochet <val.rochet@gmail.com>
2019-01-16 00:46:31 +0100

    day 12 à terminer

--- src/main/java/aoc/Main.java
+++ src/main/java/aoc/Main.java
@@ -1,13 +1,13 @@
 package aoc;
 
-import aoc.days.Day9;
+import aoc.days.Day12;
 
 public class Main {
 
     public static void main(String[] args) {
 
         try {
-            DayInterface day = new Day9();
+            DayInterface day = new Day12();
             day.part1();
             day.part2();
         } catch (Exception e) {
--- src/main/java/aoc/days/Day12.java
+++ src/main/java/aoc/days/Day12.java
@@ -5,6 +5,7 @@ import aoc.ExoEntryUtils;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,10 +24,9 @@ public class Day12 implements DayInterface {
     @Override
     public void part1() throws Exception {
         String[] entries = ExoEntryUtils.getEntries(12);
-        int nbGenerations = 20;
+        long nbGenerations = 20L;
 
         String state = entries[0].split(" ")[2];
-        int farestLeft = 0;
 
         List<Spread> spreads = new ArrayList<>();
         for (int i = 2; i < entries.length; i++) {
@@ -36,7 +36,32 @@ public class Day12 implements DayInterface {
             spreads.add(spread);
         }
 
-        for (int age = 0; age < nbGenerations; age++) {
+        System.out.println("Total points of pots on " + nbGenerations + " generations : " + generate(nbGenerations, state, spreads));
+    }
+
+    @Override
+    public void part2() throws Exception {
+        String[] entries = ExoEntryUtils.getEntries(12);
+        long nbGenerations = 50000000000L;
+
+        String state = entries[0].split(" ")[2];
+
+        List<Spread> spreads = new ArrayList<>();
+        for (int i = 2; i < entries.length; i++) {
+            Spread spread = new Spread();
+            spread.setBefore(entries[i].split(" ")[0]);
+            spread.setPlantNext(entries[i].split(" ")[2]);
+            spreads.add(spread);
+        }
+
+        System.out.println("Total points of pots on " + nbGenerations + " generations : " + generate(nbGenerations, state, spreads));
+    }
+
+    private int generate(long nbGenerations, String state, List<Spread> spreads) {
+        long farestLeft = 0, previousFarestLeft = 0;
+        long age = 0;
+
+        while (age < nbGenerations){
 
             // On étend le tableau pour qu'il y ai au minimum 4 plants vides à droite et à gauche (pour bien tester toutes les combinaisons)
             StringBuilder extendedStateBuilder = new StringBuilder();
@@ -62,13 +87,25 @@ public class Day12 implements DayInterface {
                     newState.append(".");
                 }
             }
-            state = newState.toString();
+
+            if (newState.toString().equals(state)) {
+                // On a trouvé une occurrence. On calcul l'offset entre les 2 ages, on le multiplie par le nombre d'années restantes à calculer et bingo
+                long offSet = farestLeft - previousFarestLeft;
+                farestLeft = farestLeft + offSet * (nbGenerations - age - 1);
+
+                // on va directement à la fin
+                age = nbGenerations;
+            } else {
+                state = newState.toString();
+                previousFarestLeft = farestLeft;
+                age++;
+            }
         }
 
         System.out.println("Final state after " + nbGenerations + " years : " + state);
 
         // Calcul du total des numéros de pot
-        int potValue = farestLeft;
+        long potValue = farestLeft;
         int totalPots = 0;
         for (char c : state.toCharArray()) {
             if (c == '#') {
@@ -77,66 +114,6 @@ public class Day12 implements DayInterface {
             potValue++;
         }
 
-        System.out.println("Total points of pots : " + totalPots);
-    }
-
-    @Override
-    public void part2() throws Exception {
-        String[] entries = ExoEntryUtils.getEntries(12);
-
-        String state = entries[0].split(" ")[2];
-        int farestLeft = 0;
-
-        List<Spread> spreads = new ArrayList<>();
-        for (int i = 2; i < entries.length; i++) {
-            Spread spread = new Spread();
-            spread.setBefore(entries[i].split(" ")[0]);
-            spread.setPlantNext(entries[i].split(" ")[2]);
-            spreads.add(spread);
-        }
-
-        // sur 1000000 générations on essaye de voir si il y a un pattern, un coefficient multiplicateur par age
-        for (int age = 0; age < 1000000; age++) {
-
-            // On étend le tableau pour qu'il y ai au minimum 4 plants vides à droite et à gauche (pour bien tester toutes les combinaisons)
-            StringBuilder extendedStateBuilder = new StringBuilder();
-            for (int i = 0; i < 4 - state.indexOf('#'); i++) {
-                extendedStateBuilder.append(".");
-                farestLeft--;
-            }
-            farestLeft += 2;
-            extendedStateBuilder.append(state);
-            for (int i = 0; i < 4 - state.length() + state.lastIndexOf('#') + 1; i++) {
-                extendedStateBuilder.append(".");
-            }
-
-            StringBuilder newState = new StringBuilder();
-
-            for (int i = 3; i < extendedStateBuilder.length() - 1; i++) {
-                String plantZone = extendedStateBuilder.substring(i - 3, i + 2);
-
-                Optional<Spread> optionalSpread = spreads.stream().filter(s -> s.getBefore().equals(plantZone)).findFirst();
-                if (optionalSpread.isPresent()) {
-                    newState.append(optionalSpread.get().getPlantNext());
-                } else {
-                    newState.append(".");
-                }
-            }
-            state = newState.toString();
-
-            // Calcul du total des numéros de pot
-            int potValue = farestLeft;
-            int totalPots = 0;
-            for (char c : state.toCharArray()) {
-                if (c == '#') {
-                    totalPots += potValue;
-                }
-                potValue++;
-            }
-
-            System.out.println("Coefficient : " + (double) totalPots / age);
-        }
-
-        // ça tend vers 55 mais on ne peut pas trouver de coefficient
+        return totalPots;
     }
 }
